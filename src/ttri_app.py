bf313f6bbf4d4dfb67cae50647d47c44407473d2
Alexis Lothoré <alexis.lothore@gmail.com>
2017-09-27 22:02:54 +0200

    Add USB detection to main app, make USB class generic

--- src/ttri_app.py
+++ src/ttri_app.py
@@ -1,17 +1,26 @@
 import sys
 from ttri_ui import TTRI_Ui
+from ttri_usb import TTRI_Usb
 
 class TTRI_App():
 
     def __init__(self, argv):
         self._ui = TTRI_Ui(sys.argv)
+        self._usb = TTRI_Usb(0x1390, 0x7475, 1, self.usb_status_cb)
         self._app = self._ui.get_main_app()
         if self._app is None:
             print "Error retrieving Qt main app"
             exit(1)
 
     def start(self):
+        self._usb.start()
         sys.exit(self._app.exec_())
+
+    def usb_status_cb(self, status):
+        if status == True:
+            print "Device is now connected"
+        else:
+            print "Device disconnected"
 
 if __name__ == "__main__":
     print "Initializing app"
--- src/ttri_usb.py
+++ src/ttri_usb.py
@@ -3,18 +3,30 @@ import usb.util
 from threading import Timer
 
 class TTRI_Usb():
-    def __init__(self, polling_interval=1):
+    def __init__(self, id_vendor, id_product, polling_interval=1, usb_status_cb=None):
         self._polling_interval = polling_interval
+        self._usb_status_cb = usb_status_cb
+        self._id_vendor = id_vendor
+        self._id_product = id_product
+        self._current_status = None
+        self._current_status = self.search_ttr_device()
+
+    def start(self):
         self._poll_timer = Timer(self._polling_interval, self.poll)
         self._poll_timer.start()
 
     def search_ttr_device(self):
-        dev = usb.core.find(idVendor=0x1390, idProduct=0x7475)
-        if dev is None:
-            print "Device not connected ..."
-            return False
-        print "Device connected !"
-        return True
+        status_changed = False
+        dev = usb.core.find(idVendor=self._id_vendor, idProduct=self._id_product)
+        if ((dev is None and self._current_status == True) or
+                (dev is not None and self._current_status == False)):
+            status_changed = True
+        self._current_status = (dev != None)
+        if(status_changed):
+            self._usb_status_cb(self._current_status)
+
+        return (dev != None)
+
 
     def poll(self):
         self.search_ttr_device()
@@ -24,4 +36,5 @@ class TTRI_Usb():
 if __name__ == "__main__":
     print "Starting USB test"
     app = TTRI_Usb()
+    app.start()
 
